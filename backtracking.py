4f5889f42077f53adf5a8e5f70bcb0bf1f4dac30
Rathetsu <ah.ezzat.muh@gmail.com>
2020-05-13 03:07:45 +0200

    finished main algorthim and testing

---
+++ backtracking.py
@@ -0,0 +1,27 @@
+from solve import *
+
+
+# The main driver and the backtracking function
+def backtracking(game):
+    empty = next_empty(game)
+    if not(empty):                  # Base case: We're done once there're no empty squares left.
+        return True
+    else:
+        r, c = empty
+    for i in range(1, 10):
+        if is_valid(game, i, (r, c)):
+            game[r][c] = i
+            #print("a7a")
+
+            if backtracking(game):
+                #print("5555555")
+                return True
+            else:
+                game[r][c] = 0
+
+    return False
+
+print_game(game)
+print("\n___________________________\n")
+backtracking(game[:])
+print_game(game)
--- solve.py
+++ solve.py
@@ -3,15 +3,15 @@ from math import *
 
 # A test case that will be replaced by the output of the generating function once the algorithm is tested
 game = [
-    [4,"_",7,"_",8,6,1,9,"_"],
-    ["_",1,5,3,"_","_","_",6,"_"],
-    [6,"_",3,"_","_","_",5,2,"_"],
-    ["_","_","_","_",6,2,4,3,"_"],
-    ["_",4,"_",8,"_",5,"_","_","_"],
-    [2,"_",6,"_",3,"_",9,"_",1],
-    [1,"_","_","_",5,8,"_",4,9],
-    [5,"_","_",7,4,"_",6,"_","_"],
-    ["_",6,"_","_",9,"_","_",5,"_"]
+    [4,0,7,0,8,6,1,9,0],
+    [0,1,5,3,0,0,0,6,0],
+    [6,0,3,0,0,0,5,2,0],
+    [0,0,0,0,6,2,4,3,0],
+    [0,4,0,8,0,5,0,0,0],
+    [2,0,6,0,3,0,9,0,1],
+    [1,0,0,0,5,8,0,4,9],
+    [5,0,0,7,4,0,6,0,0],
+    [0,6,0,0,9,0,0,5,0]
 ]
 
 # A function that prints out the test case above in console
@@ -28,14 +28,40 @@ def print_game(game):
             else:
                 print(str(game[i][j]) + " ", end = "")
 
-# print_game(game)
 
-# A function that finds the next empty square and returns the location in the form of a tuple (col, row)
+# A function that finds the next empty square and returns the position in the form of a tuple (col, row)
 def next_empty(game):
-    for i in range len(game):
-        for j in range len(game[0]:
-            if game[i][j] == "_":
-                return (j, i)       # (col, row)
+    for i in range(len(game)):   
+        for j in range(len(game[0])):
+            if game[i][j] == 0:
+                return (i, j)       # (r, c)
+    return False
+
+
+#A function that checks if the entered number is valid
+def is_valid(game, number, position):
+    #row
+    for i in range(9):  
+        if game[position[0]][i] == number and position[1] != i:
+            return False
+    #column
+    for i in range(9):
+        if game[i][position[1]] == number and position[0] != i:
+            return False
+    #box
+    x = position[1] // 3
+    y = position[0] // 3
+    for i in range(y*3, y*3 + 3):
+        for j in range(x*3, x*3 + 3):
+            if game[i][j] == number and (i, j) != position:
+                return False
+    return True
+
+
+
+
+
+        
 
 
     
